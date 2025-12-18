8e0c1f4586d80004f736964403e7a34ebf261efb
Astoritin <Astoritin@outlook.com>
2025-12-18 23:29:57 +0800

    Remove recognizing Kitsune Mask/Magisk Delta/Magisk Lite codes Since these root implements will not be updated anymore

--- module/wanderer.sh
+++ module/wanderer.sh
@@ -8,9 +8,6 @@ is_magisk() {
     MAGISK_V_VER_CODE="$(magisk -V)"
     case "$MAGISK_V_VER_NAME" in
         *"-alpha"*) MAGISK_BRANCH_NAME="Alpha" ;;
-        *"-lite"*)  MAGISK_BRANCH_NAME="Magisk Lite" ;;
-        *"-kitsune"*) MAGISK_BRANCH_NAME="Kitsune Mask" ;;
-        *"-delta"*) MAGISK_BRANCH_NAME="Magisk Delta" ;;
         *) MAGISK_BRANCH_NAME="Magisk" ;;
     esac
     DETECT_MAGISK="true"

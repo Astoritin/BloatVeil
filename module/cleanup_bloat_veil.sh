e48a07099bc1dcc20e2939ef06cb0bbd240c1c01
Astoritin <Astoritin@outlook.com>
2025-12-18 23:40:59 +0800

    Remove dealing with /data/adb/lite_modules/ Magisk Lite is not supported anymore

--- module/cleanup_bloat_veil.sh
+++ module/cleanup_bloat_veil.sh
@@ -12,18 +12,15 @@ POST_D="/data/adb/post-fs-data.d/"
 CLEANUP_SH="cleanup_bloat_veil.sh"
 CLEANUP_PATH="${POST_D}/${CLEANUP_SH}"
 
-MOD_DIR="/data/adb/modules/bloat_veil"
-LITE_MOD_DIR="/data/adb/lite_modules/bloat_veil"
-MIRROR_DIR="$MOD_DIR/mirror"
-MIRROR_SYSTEM_DIR="$MOD_DIR/system"
+MODDIR="/data/adb/modules/bloat_veil"
+MIRROR_DIR="$MODDIR/mirror"
+MIRROR_SYSTEM_DIR="$MODDIR/system"
 
-. "$MOD_DIR/wanderer.sh"
+. "$MODDIR/wanderer.sh"
 
 if [ ! -f "$FLAG_BRICKED" ]; then
-    if [ -f "$MOD_DIR/disable" ]; then
-        update_config_var "description" "$MOD_DIR/module.prop" "$DESCRIPTION"
-    elif [ -f "$LITE_MOD_DIR/disable" ]; then
-        update_config_var "description" "$LITE_MOD_DIR/module.prop" "$DESCRIPTION"
+    if [ -f "$MODDIR/disable" ]; then
+        update_config_var "description" "$MODDIR/module.prop" "$DESCRIPTION"
     fi
 fi
 

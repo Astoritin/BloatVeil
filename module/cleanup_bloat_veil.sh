438efe432f5d337e9bf847383d09d1839d2c616e
Astoritin <Astoritin@outlook.com>
2026-01-12 23:17:52 +0800

    Try to support last worked target list as module disabled

--- module/cleanup_bloat_veil.sh
+++ module/cleanup_bloat_veil.sh
@@ -1,31 +1,29 @@
 #!/system/bin/sh
 
-update_key_value() {
-    key="$1"
-    conf="$2"
-    expected="$3"
-    append="${4:-false}"
-
-    [ -z "$key" ] || [ -z "$expected" ] || [ -z "$conf" ] || [ ! -f "$conf" ] && return 1
-
-    if grep -q "^${key}=" "$conf"; then
-        [ "$append" = true ] && return 0
-        sed -i "/^${key}=/c\\${key}=${expected}" "$conf"
-    else
-        [ -n "$(tail -c1 "$conf")" ] && echo >> "$conf"
-        printf '%s=%s\n' "$key" "$expected" >> "$conf"
-    fi
-}
-
 MODS_DIR="/data/adb/modules"
 [ -n "$(magisk -v | grep lite)" ] && MODS_DIR="/data/adb/lite_modules"
 
 MOD_NAME="bloat_veil"
 MOD_DESC="A bloatware vanishing act on the system."
-
 MOD_DIR="$MODS_DIR/$MOD_NAME"
-[ -f "$MOD_DIR/disable" ] && update_key_value "description" "$MOD_DIR/module.prop" "$MOD_DESC"
+
+CONFIG_DIR="/data/adb/bloat_veil"
+TARGET_LIST_BVA="$CONFIG_DIR/targets_bva.txt"
+FLAG_BRICKED="$CONFIG_DIR/bricked"
+
+. "$MOD_DIR/wanderer.sh"
+
+brick_rescue=$(get_key_value "brick_rescue" "$CONFIG_FILE") || brick_rescue=true
+brick_and_disable=$(get_key_value "brick_and_disable" "$CONFIG_FILE") || brick_and_disable=true
+last_worked_target_list=$(get_key_value "last_worked_target_list" "$CONFIG_FILE") || last_worked_target_list=true
+
+if [ -f "$FLAG_BRICKED" ]; then
+    update_key_value "description" "$MOD_DIR/module.prop" "$MOD_DESC"
+fi
+
+    
+
 rm -f "$MOD_DIR/mirror" "$MOD_DIR/system"
 
-rm -f "/data/adb/bloat_veil/logs/targets_bva.txt"
+rm -f "$TARGET_LIST_BVA"
 rm -f "/data/adb/post-fs-data.d/cleanup_bloat_veil.sh"

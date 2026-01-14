24a04c8267ff41433a0279b7cd63329e8ec45799
Astoritin <Astoritin@outlook.com>
2026-01-14 11:58:21 +0800

    No more introducing wanderer.sh in post-fs-data.d stage

--- module/bloat_veil_unbrick.sh
+++ module/bloat_veil_unbrick.sh
@@ -7,8 +7,6 @@ MOD_ID="bloat_veil"
 MOD_DESC="A bloatware vanishing act on the system."
 MOD_DIR="$MODS_DIR/$MOD_ID"
 
-. "$MOD_DIR/wanderer.sh"
-
 CONFIG_DIR="/data/adb/bloat_veil"
 
 CONFIG_FILE="$CONFIG_DIR/settings.conf"
@@ -21,6 +19,61 @@ TARGET_LIST_LW="$LAST_WORKED_DIR/targets_lw.txt"
 
 MIRROR_DIR="$MOD_DIR/mirror"
 MIRROR_SYSTEM_DIR="$MOD_DIR/system"
+
+get_key_value() {
+    local key=$1
+    local conf=$2
+    [ -n "$key" ] && [ -f "$conf" ] || return 1
+
+    awk -v key="$key" '
+        BEGIN { key_regex = "^" key "[[:space:]]*=" }
+        $0 ~ key_regex {
+            sub(key_regex, "")
+            sub(/^[[:space:]]*/, "")
+            if (sub(/^"/, "")) {
+                if (!sub(/"[[:space:]]*$/, "")) exit 1
+            } else {
+                sub(/[[:space:]]+$/, "")
+            }
+            print
+            exit 0
+        }
+        END { exit 1 }
+    ' "$conf" | tr -d '\r'
+
+    [ $? -eq 0 ] || return 1
+}
+
+update_key_value() {
+    local key="$1"
+    local conf="$2"
+    local expected="$3"
+    local append="${4:-false}"
+
+    [ -z "$key" ] || [ -z "$expected" ] || [ -z "$conf" ] || [ ! -f "$conf" ] && return 1
+
+    if grep -q "^${key}=" "$conf"; then
+        [ "$append" = true ] && return 0
+        sed -i "/^${key}=/c\\${key}=${expected}" "$conf"
+    else
+        [ -n "$(tail -c1 "$conf")" ] && echo >> "$conf"
+        printf '%s=%s\n' "$key" "$expected" >> "$conf"
+    fi
+}
+
+file_compare() {
+    local file_a="$1"
+    local file_b="$2"
+    
+    [ -z "$file_a" ] || [ ! -f "$file_a" ] && return 2
+    [ -z "$file_b" ] || [ ! -f "$file_b" ] && return 3
+    
+    hash_file_a=$(sha256sum "$file_a" | awk '{print $1}')
+    hash_file_b=$(sha256sum "$file_b" | awk '{print $1}')
+    
+    [ "$hash_file_a" = "$hash_file_b" ] && return 0
+    [ "$hash_file_a" != "$hash_file_b" ] && return 1
+}
 
 config_loader() {
 
